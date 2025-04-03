2bdcd1146763af664b6aeeed0303f8c080b77b90
Eclairsombre <alexandre.thny@gmail.com>
2025-04-03 22:32:16 +0200

    Update database config

--- backend/Models/Student.cs
+++ backend/Models/Student.cs
@@ -5,7 +5,7 @@ namespace backend.Models
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
 
-        public string Surname { get; set; } = string.Empty;
+        public string Firstname { get; set; } = string.Empty;
 
         public string StudentNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
