69c5d4c0db1ca00326954b9c953fe0098bb642b4
eymeric <eymericdechelette@gmail.com>
2025-05-04 23:03:05 +0200

    Add configurable thread count to HTTP server script

--- front/default.nix
+++ front/default.nix
@@ -21,7 +21,8 @@ buildNpmPackage rec {
     mkdir -p $out/bin
     echo '#!${bash}/bin/bash' > $out/bin/${pname}
     echo 'PORT=''${PORT:-8000}' >> $out/bin/${pname}
-    echo '${simple-http-server}/bin/simple-http-server -i -p $PORT ${placeholder "out"}/lib' >> $out/bin/${pname}
+    echo 'THREADS=''${THREADS:-3}' >> $out/bin/${pname}
+    echo '${simple-http-server}/bin/simple-http-server -i -p $PORT -t $THEADS ${placeholder "out"}/lib' >> $out/bin/${pname}
     chmod +x $out/bin/${pname}
     runHook postInstall
   '';
